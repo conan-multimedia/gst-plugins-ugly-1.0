faf5d2e1471690a65f6d95180a7e7f5b51e5299e
cppbitman <chinadaihongjun@gmail.com>
2018-11-08 21:04:33 -0800

    remove unneeded comments

--- conanfile.py
+++ conanfile.py
@@ -12,16 +12,6 @@ class GstpluginsuglyConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=True"
     generators = "cmake"
-    #requires = ("a52dec/0.7.4@user/channel","bzip2/1.0.6@user/channel","expat/2.2.5@user/channel",
-    #            "gtk-doc-lite/1.27@user/channel","libdvdread/5.0.0@user/channel","libffi/3.99999@user/channel",
-    #            "libjpeg-turbo/1.5.3@user/channel","libmpeg2/0.5.1@user/channel","libogg/1.3.3@user/channel",
-    #            "libvisual/0.4.0@user/channel","opencore-amr/0.1.5@user/channel","opus/1.2.1@user/channel",
-    #            "orc/0.4.28@bincrafters/stable","pixman/0.34.0@user/channel","x264/20161218-2245@user/channel",
-    #            "zlib/1.2.11@user/channel","glib/2.54.3@bincrafters/stable","libpng/1.6.34@user/channel",
-    #            "libvorbis/1.3.5@user/channel","libxml2/2.9.7@user/channel","freetype/2.9@user/channel",
-    #            "gobject-introspection/1.54.1@bincrafters/stable","libtheora/1.1.1@user/channel","fontconfig/2.12.6@user/channel",
-    #            "graphene/1.4.0@bincrafters/stable","gstreamer/1.14.3@bincrafters/stable","cairo/1.14.12@bincrafters/stable",
-    #            "harfbuzz/1.7.5@bincrafters/stable","pango/1.40.14@bincrafters/stable","gst-plugins-base/1.14.3@bincrafters/stable",)
     requires = ("gstreamer-1.0/1.14.4@conanos/dev","gst-plugins-base-1.0/1.14.4@conanos/dev","a52dec/0.7.4@conanos/dev",
                 "opencore-amr/0.1.5@conanos/dev","libdvdread/5.0.0@conanos/dev","libmpeg2/0.5.1@conanos/dev","x264/20181108-2245@conanos/dev",
                 "orc/0.4.28@conanos/dev")
@@ -34,11 +24,6 @@ class GstpluginsuglyConan(ConanFile):
         os.rename(extracted_dir, self.source_subfolder)
 
     def build(self):
-        #vars = {'C_INCLUDE_PATH': '%s/include'%(self.deps_cpp_info["opencore-amr"].rootpath),
-        #        'LD_LIBRARY_PATH':'%s/lib:%s/lib'%(self.deps_cpp_info["libffi"].rootpath,self.deps_cpp_info["glib"].rootpath)
-        #       }
-        #with tools.environment_append(vars):
-
         with tools.chdir(self.source_subfolder):
             with tools.environment_append({
                 'LIBRARY_PATH' : '%s/lib'%(self.deps_cpp_info["a52dec"].rootpath),
